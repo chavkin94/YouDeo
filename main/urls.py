0e4baa9b0681aeca49af0ed407f276fdc05e156e
Acer <chavkin94@gmail.com>
2022-04-26 09:27:29 +0900

    0

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,10 @@ from . import views
 from django.contrib.auth.views import auth_login
 
 urlpatterns = [
-    path('', views.base, name='home'),
+    path(r'', views.base, name='home'),
+    path(r'account/', views.account, name='account'),
+    path(r'feed/', views.feed, name='feed'),
+    path(r'message/', views.message, name='message'),
+    path(r'master/', views.master, name='master'),
+    path(r'organization/', views.organization, name='organization'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,39 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
+
+menu = [{'title': "Публикации", 'url_name': 'lenta'},
+        {'title': "Поиск мастера", 'url_name': 'search'},
+        {'title': "Сообщения", 'url_name': 'message'},
+        {'title': "Профиль", 'url_name': 'account'},
+        {'title': "Профиль мастера", 'url_name': 'master'},
+        {'title': "Профиль организации", 'url_name': 'organization'}
+        ]
+
+
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
 
 def base(request):
-    return render(request, 'main/home.html')
+    return render(request, 'main/content/home.html', {'title': 'Поиск мастера'})
+
+
+def account(request):
+    return render(request, 'main/content/account.html', {'title': 'Профиль'})
+
+
+def feed(request):
+    return render(request, 'main/content/feed.html', {'title': 'Публикации'})
+
+
+def message(request):
+    return render(request, 'main/content/message.html', {'title': 'Сообщения'})
+
+
+def master(request):
+    return render(request, 'main/content/master.html', {'title': 'Профиль мастера'})
+
+
+def organization(request):
+    return render(request, 'main/content/organization.html', {'title': 'Профиль организации'})
