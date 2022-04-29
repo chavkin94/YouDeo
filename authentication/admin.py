e49e590ef2d4d85a1e67fa9c4a7f6073988fd968
Acer <chavkin94@gmail.com>
2022-04-29 09:46:17 +0900

    регистрация готово, добавить стили

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-
 from authentication.models import *
+
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'is_active')
-    # list_display_links = ('') #{ поля на которые можно кликать}#
-    # search_fields = ('fk_user') #{ по каким полям можно производить поиск}#
-    # list_filter = ('fk_user', 'time_create', 'time_update') #{поля по которым можно сортировать админку}#
+    list_display_links = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'is_active')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'is_active')
 
 
 admin.site.register(User, UserAdmin)
 
-
-# admin.site.register(User)
--- authentication/views.py
+++ authentication/views.py
@@ -18,16 +18,15 @@ def registration(request):
     return HttpResponse("Регистрация")
 
 
-# class RegisterUser(DataMixin, CreateView):
-#     # form_class = UserCreationForm
-#     # form_class = RegisterUserForm
-#     template_name = 'authentication/registration.html'
-#     success_url = reverse_lazy('login')
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         c_def = self.get_user_context(title='Регистрация')
-#         return dict(list(context.items()) + list(c_def.items()))
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'authentication/registration.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 # @login_required
