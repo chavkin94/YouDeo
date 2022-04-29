62bb0a706b294d2d413bed84b435bf1e9dcefe48
Acer <chavkin94@gmail.com>
2022-04-29 11:28:47 +0900

    .

--- authentication/forms.py
+++ authentication/forms.py
@@ -40,14 +40,11 @@ from authentication.models import *
 
 
 class RegisterUserForm(UserCreationForm):
-    # штуки для того чтобы сделать стильными поля в форме
-    gender_select = [(gender, gender) for gender in ['Не выбрано', 'Мужской', 'Женский']]
-    # gender_select =
     username = forms.CharField(label='Логин на сайте', widget=forms.TextInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'form-control'}))
     birthday = forms.DateField(label='Дата рождения', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
-    gender = forms.ChoiceField(label='Пол', widget=forms.Select(attrs={'class': 'form-control'}), choices=gender_select)
+    gender = forms.ChoiceField(label='Пол', widget=forms.Select(attrs={'class': 'form-control'}), choices=(('', ""), ('м', "мужской"), ('ж', "женский")))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     phone_number = forms.CharField(label='Номер телефона', widget=forms.TextInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
